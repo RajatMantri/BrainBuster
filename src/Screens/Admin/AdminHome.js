7d2bbc8579c45d8b95d3d7ef2d45457f13d70766
piyushsahu7873 <127032938+piyushsahu7873@users.noreply.github.com>
2024-07-08 02:15:25 +0530

    Updated Admin and Student Portals

--- src/Screens/Admin/AdminHome.js
+++ src/Screens/Admin/AdminHome.js
@@ -1,47 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "../../Components/NavBar";
 import Footer from "../../Components/Footer";
 import NotFound from "../../Components/NotFound";
-
+import axios from 'axios';
 const AdminHome = () => {
-    const type=localStorage.getItem('type');
-    const recentQuizzes = [
-        { name: "Quiz 1", score: 80 },
-        { name: "Quiz 2", score: 75 },
-        { name: "Quiz 3", score: 90 }
-    ];
+    const type = localStorage.getItem('type');
+    const username = localStorage.getItem('username');
+    const [quizzes, setQuizzes] = useState([]);
+
+    useEffect(() => {
+        fetchQuizzes();
+    }, []);
+
+    const fetchQuizzes = async () => {
+        try {
+            const response = await axios.get(`http://localhost:4000/api/quizzes/show/${username}`);
+            setQuizzes(response.data);
+        } catch (error) {
+            console.error('Error fetching quizzes:', error);
+        }
+    };
+
+    let recentQuizzes = [];
+    if (quizzes.length >= 3) {
+        recentQuizzes = quizzes.slice(-3).reverse();
+    } else if (quizzes.length === 2) {
+        recentQuizzes = quizzes.slice(-2).reverse();
+    } else {
+        recentQuizzes = quizzes;
+    }
+
+    console.log(recentQuizzes);
 
     return (
         <>
-            {localStorage.getItem('username')&&type==="admin" ? (
+            {localStorage.getItem('username') && type === "admin" ? (
                 <div>
                     <NavBar type="admin" username={localStorage.getItem('username')} />
                     <div className="jumbotron">
                         <h1><u>Welcome {localStorage.getItem('username')}!</u></h1>
+                        <h2>Recent Attempted Quizzes</h2>
                     </div>
 
                     <div className="features">
-                        <div className="feature">
-                            <img src="https://via.placeholder.com/200" alt="Feature Image 1" />
-                            <div className="feature-content">
-                                <h3 className="feature-title">Interactive Quizzes</h3>
-                                <p className="feature-description">Engage users with interactive quizzes that test their knowledge.</p>
+                        {
+                            recentQuizzes.map((quiz) => (
+                            <div className="feature" key={quiz._id}>
+                                <a href={`http://localhost:3000/adminHome/quiz/${quiz._id}`}>
+                                    <img src="https://static.vecteezy.com/system/resources/previews/005/083/209/non_2x/editable-flat-outline-design-of-quiz-icon-vector.jpg" alt="Feature Image" />
+                                    <div className="feature-content">
+                                        <h3 className="feature-title">{quiz.title}</h3>
+                                        <p className="feature-description">{quiz.description}</p>
+                                    </div>
+                                </a>
                             </div>
-                        </div>
-                        <div className="feature">
-                            <img src="https://via.placeholder.com/200" alt="Feature Image 2" />
-                            <div className="feature-content">
-                                <h3 className="feature-title">Customizable Themes</h3>
-                                <p className="feature-description">Choose from a variety of themes to customize the appearance of quizzes.</p>
-                            </div>
-                        </div>
-                        <div className="feature">
-                            <img src="https://via.placeholder.com/200" alt="Feature Image 3" />
-                            <div className="feature-content">
-                                <h3 className="feature-title">Leaderboards</h3>
-                                <p className="feature-description">Track users' performance and display top scores on leaderboards.</p>
-                            </div>
-                        </div>
+                        ))}
                     </div>
                     <Footer />
                 </div>
--- src/Screens/Student/StudentHome.js
+++ src/Screens/Student/StudentHome.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import NavBar from "../../Components/NavBar";
 import Footer from "../../Components/Footer";
 import NotFound from "../../Components/NotFound";
+import axios from "axios";
 
 const StudentHome = () => {
+    const [attemptedQuizzes, setAttemptedQuizzes] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const username = localStorage.getItem('username');
     const type = localStorage.getItem('type');
+
+    useEffect(() => {
+        const fetchAttemptedQuizzes = async () => {
+            try {
+                const response = await axios.get(`http://localhost:4000/api/getAttemptedQuizzes/${username}`);
+                setAttemptedQuizzes(response.data);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching attempted quizzes:', error);
+                setLoading(false);
+            }
+        };
+
+        fetchAttemptedQuizzes();
+    }, [username]);
+
+    let recentQuizzes = [];
+    if (attemptedQuizzes.length >= 3) {
+        recentQuizzes = attemptedQuizzes.slice(-3).reverse();
+    } else if (attemptedQuizzes.length === 2) {
+        recentQuizzes = attemptedQuizzes.slice(-2).reverse();
+    } else {
+        recentQuizzes = attemptedQuizzes;
+    }
+
     return (
         <>
             {localStorage.getItem('username') && type === "student" ? (
@@ -12,32 +42,19 @@ const StudentHome = () => {
                     <NavBar type="student" username={localStorage.getItem('username')} />
                     <div className="jumbotron">
                         <h1><u>Welcome {localStorage.getItem('username')}!</u></h1>
+                        <h2>Recent Attempted Quizzes</h2>
                     </div>
                     <div className="features">
-                        <div className="feature">
-                            <img src="https://via.placeholder.com/200" alt="Feature Image 1" />
-                            <div className="feature-content">
-                                <button>Retake Quiz</button>
-                                <br />
-                                <button>See Results</button>
+                        {attemptedQuizzes.map((data) => (
+                            <div className="feature" key={data._id}>
+                                <img src="https://static.vecteezy.com/system/resources/previews/005/083/209/non_2x/editable-flat-outline-design-of-quiz-icon-vector.jpg" alt="Feature Image 1" />
+                                <div className="feature-content">
+                                    <Link to={`/quiz/${data.quizId}/attempt`}><button>Retake Quiz</button></Link>
+                                    <br />
+                                    <Link to={`/quiz/${data.quizId}/result`}><button>See Result</button></Link>
+                                </div>
                             </div>
-                        </div>
-                        <div className="feature">
-                            <img src="https://via.placeholder.com/200" alt="Feature Image 2" />
-                            <div className="feature-content">
-                                <button>Retake Quiz</button>
-                                <br />
-                                <button>See Results</button>
-                            </div>
-                        </div>
-                        <div className="feature">
-                            <img src="https://via.placeholder.com/200" alt="Feature Image 3" />
-                            <div className="feature-content">
-                                <button>Retake Quiz</button>
-                                <br />
-                                <button>See Results</button>
-                            </div>
-                        </div>
+                        ))}
                     </div>
                     <Footer />
                 </div>
