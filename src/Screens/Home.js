ee847f4fb073e1f8dcb510273793e3dd43f8aeb6
piyushsahu7873 <127032938+piyushsahu7873@users.noreply.github.com>
2024-07-10 19:47:21 +0530

    Updated Home page

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -12,21 +12,21 @@ const Home = (props) => {
 
         <div className="features">
             <div className="feature">
-                <img src="https://via.placeholder.com/200" alt="Feature Image 1" />
+                <img src="https://thumbs.dreamstime.com/b/male-hand-holding-megaphone-quiz-time-speech-bubble-loudspeaker-banner-business-marketing-advertising-vector-125104939.jpg" alt="Feature Image 1" />
                 <div className="feature-content">
                     <h3 className="feature-title">Interactive Quizzes</h3>
                     <p className="feature-description">Engage users with interactive quizzes that test their knowledge.</p>
                 </div>
             </div>
             <div className="feature">
-                <img src="https://via.placeholder.com/200" alt="Feature Image 2" />
+                <img src="https://png.pngtree.com/png-clipart/20210309/original/pngtree-practice-makes-a-man-perfect-tshirt-png-image_5839403.jpg" alt="Feature Image 2" />
                 <div className="feature-content">
-                    <h3 className="feature-title">Customizable Themes</h3>
-                    <p className="feature-description">Choose from a variety of themes to customize the appearance of quizzes.</p>
+                    <h3 className="feature-title">Retake Quizzes</h3>
+                    <p className="feature-description">Retake recent quizzes, correct your mistakes.</p>
                 </div>
             </div>
             <div className="feature">
-                <img src="https://via.placeholder.com/200" alt="Feature Image 3" />
+                <img src="https://t3.ftcdn.net/jpg/04/21/44/74/360_F_421447468_CXRbXXka6BdfvmIZju8rezIoGewUQ6BT.jpg" alt="Feature Image 3" />
                 <div className="feature-content">
                     <h3 className="feature-title">Leaderboards</h3>
                     <p className="feature-description">Track users' performance and display top scores on leaderboards.</p>
